a2f6fc263a1361b0c83ea7fb904163067ad2ab01
mahenq <90361623+mahenq@users.noreply.github.com>
2025-07-27 13:55:23 +0700

    srv

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.get("/ping", (req, res) => res.json({ ping: "pong", time: Date.now() }));
 app.use("/api/admins", adminRoutes);
 app.use("/api/customers", customerRoutes);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
